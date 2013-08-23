cc54bce13f367043a14f35c08a597496a450979d
Andrew Johnson <johnsonap@gmail.com>
2013-08-23 10:46:10 -0500

    added hvac channel handler to pyclient.py

--- pyclient.py
+++ pyclient.py
@@ -12,9 +12,16 @@ def lights_callback(data):
         status = '1'
     arduino.write('lght' + json_data['light_id'] + status)
     
+def hvac_callback(data):
+    # TODO - update hvac settings for
+    # when temp is updated and when
+    # settings are updated
+    
 def connect_handler(data):
-    channel = pusher.subscribe("lights")
-    channel.bind('update_lights', lights_callback)
+    lights_channel = pusher.subscribe("lights") 
+    hvac_channel = pusher.subscribe("lights")
+    lights_channel.bind('update_lights', lights_callback)
+    hvac_channel.bind('update_settings', hvac_callback)
     
 if __name__ == '__main__':
     pusher = pusherclient.Pusher('bbfd2fdfc81124a36b18')
